90eeff962527dcc88c91ab30cf33423b16e983c0
冯伟 <lyman.von9506@gmail.com>
2022-08-26 08:34:54 +0800

    test: 复合类型/示例

--- composite_data_types/src/main.rs
+++ composite_data_types/src/main.rs
@@ -1,3 +1,70 @@
+#[allow(unused_variables)]
+
+static mut ERROR: i32 = 0;
+
+#[derive(Debug)]
+struct File {
+    name: String,
+    data: Vec<u8>,
+}
+
+impl File {
+    fn new(name: &str) -> File {
+        File {
+            name: String::from(name),
+            data: Vec::new(),
+        }
+    }
+
+    fn new_with_data(name: &str, data: Vec<u8>) -> File {
+        let mut f = File::new(name);
+        f.data = data.clone();
+
+        f
+    }
+}
+
+fn open(f: &mut File) -> bool {
+    true
+}
+
+fn close(f: &mut File) -> bool {
+    true
+}
+
+fn read(f: &File, save_to: &mut Vec<u8>) -> usize {
+    let mut tmp = f.data.clone();
+    let read_length = tmp.len();
+
+    save_to.reserve(read_length);
+    save_to.append(&mut tmp);
+
+    read_length
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut f2 = File {
+        name: String::from("2.txt"),
+        data: vec![114, 117, 115, 116, 33],
+    };
+
+    let mut buffer: Vec<u8> = vec![];
+
+    open(&mut f2);
+    let f2_length = read(&f2, &mut buffer);
+
+    close(&mut f2);
+
+    let text = String::from_utf8_lossy(&buffer);
+
+    println!("{:?}", f2);
+    println!("{} is {} bytes long", f2.name, f2_length);
+    println!("{}", text);
+
+    let f3 = File::new("f3.txt");
+    let f3_name = &f3.name;
+    let f3_length = &f3.data.len();
+
+    println!("{:?}", f3);
+    println!("{} is {} bytes long", f3_name, f3_length);
 }
