6f727ad56315dae0a82bd78cfcdcb5f0616309d1
冯伟 <lyman.von9506@gmail.com>
2022-08-29 22:28:38 +0800

    feat: 完善复合类型/添加trait

--- composite_data_types/src/main.rs
+++ composite_data_types/src/main.rs
@@ -1,11 +1,20 @@
-#[allow(unused_variables)]
+use rand::prelude::*;
 
-static mut ERROR: i32 = 0;
+fn one_in(denominator: u32) -> bool {
+    thread_rng().gen_ratio(1, denominator) // 创建线程局部随机数生成器，以n/m的概率返回一个bool值
+}
+
+#[derive(Debug, PartialEq)]
+enum FileState {
+    Open,
+    Closed,
+}
 
 #[derive(Debug)]
 struct File {
     name: String,
     data: Vec<u8>,
+    state: FileState,
 }
 
 impl File {
@@ -13,58 +22,62 @@ impl File {
         File {
             name: String::from(name),
             data: Vec::new(),
+            state: FileState::Closed,
         }
     }
 
-    fn new_with_data(name: &str, data: Vec<u8>) -> File {
+    fn new_with_data(name: &str, data: &Vec<u8>) -> File {
         let mut f = File::new(name);
         f.data = data.clone();
 
         f
     }
+
+    fn read(self: &File, save_to: &mut Vec<u8>) -> Result<usize, String> {
+        let mut tmp = self.data.clone();
+        let read_length = tmp.len();
+        save_to.reserve(read_length);
+        save_to.append(&mut tmp);
+
+        Ok(read_length)
+    }
 }
 
-fn open(f: &mut File) -> bool {
-    true
+fn open(mut f: File) -> Result<File, String> {
+    if one_in(10_000) {
+        let err_msg = String::from("Permission denied");
+        return Err(err_msg);
+    }
+
+    f.state = FileState::Open;
+
+    Ok(f)
 }
 
-fn close(f: &mut File) -> bool {
-    true
-}
+fn close(mut f: File) -> Result<File, String> {
+    if one_in(100_000) {
+        let err_msg = String::from("Interrupted by signal");
+        return Err(err_msg);
+    }
 
-fn read(f: &File, save_to: &mut Vec<u8>) -> usize {
-    let mut tmp = f.data.clone();
-    let read_length = tmp.len();
+    f.state = FileState::Closed;
 
-    save_to.reserve(read_length);
-    save_to.append(&mut tmp);
-
-    read_length
+    Ok(f)
 }
 
 fn main() {
-    let mut f2 = File {
-        name: String::from("2.txt"),
-        data: vec![114, 117, 115, 116, 33],
-    };
+    let f4_data = vec![114, 117, 115, 116, 31];
+    let mut f4 = File::new_with_data("4.txt", &f4_data);
 
-    let mut buffer: Vec<u8> = vec![];
+    let mut buffer = vec![];
 
-    open(&mut f2);
-    let f2_length = read(&f2, &mut buffer);
-
-    close(&mut f2);
+    f4 = open(f4).unwrap();
+    let f4_len = f4.read(&mut buffer).unwrap();
+    f4 = close(f4).unwrap();
 
     let text = String::from_utf8_lossy(&buffer);
 
-    println!("{:?}", f2);
-    println!("{} is {} bytes long", f2.name, f2_length);
+    println!("{:?}", f4);
+    println!("{} is {} bytes long", &f4.name, f4_len);
     println!("{}", text);
-
-    let f3 = File::new("f3.txt");
-    let f3_name = &f3.name;
-    let f3_length = &f3.data.len();
-
-    println!("{:?}", f3);
-    println!("{} is {} bytes long", f3_name, f3_length);
 }
--- globalerror/src/main.rs
+++ globalerror/src/main.rs
@@ -1,27 +1,29 @@
-use rand::{random};            // <1>
+use rand::random; // 引入mod包到当前作用域
 
-static mut ERROR: isize = 0;   // <2>
+static mut ERROR: isize = 0; // 全局静态可变量，拥有静态生命周期。初始化为0
 
-struct File;                   // <3>
+struct File;
 
 #[allow(unused_variables)]
 fn read(f: &File, save_to: &mut Vec<u8>) -> usize {
-    if random() && random() && random() {   // <4>
+    if random() && random() && random() {
+        // 八分之一的概率返回true
         unsafe {
-            ERROR = 1;                      // <5>
+            ERROR = 1;
         }
     }
 
-    0                          // <6>
+    0 // <6>
 }
 
-#[allow(unused_mut)]           // <7>
+#[allow(unused_mut)]
 fn main() {
     let mut f = File;
     let mut buffer = vec![];
 
     read(&f, &mut buffer);
-    unsafe {   // <8>
+    unsafe {
+        // 访问并修改可变静态变量，就必须使用unsafe块
         if ERROR != 0 {
             panic!("An error has occurred!")
         }
